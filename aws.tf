6ebff29bb0ba5efc9f3b2b692b58291a1e1db59b
kmdcloudtech <93834713+kmdcloudtech@users.noreply.github.com>
2022-09-29 14:52:27 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "jenkins-demo" {
     ami = "ami-026b57f3c383c2eec"
     instance_type = "t2.micro"
     count = "1"
-    key_name = "demo-key"
+    key_name = "jenkins-key"
 
     tags = {
       "Name" = "jenkins-demo"
